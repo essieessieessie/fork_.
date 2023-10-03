169ba2e7ead7e22876d09ec53b568d5985d2cf6c
essieessieessie <146861780+essieessieessie@users.noreply.github.com>
2023-10-03 12:00:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Esmhan Awadh"; // update the part BETWEEN the "quotes"
+let userName = "Esmhan Awadh"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
